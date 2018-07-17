cb6bcf57c5ec207632c1d8d759e335d3a9bc3b8e
Eric Haskins <eric@ehaskins.com>
2018-07-17 00:48:38 -0500

    Add Google Analytics

--- blog/src/gatsby-config.js
+++ blog/src/gatsby-config.js
@@ -7,6 +7,20 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-122477497-1",
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/../content/posts`,
